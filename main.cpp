7029628a9a0d53bc8dbe6e41afdb7bce9f31d80c
user <ioanamarica66@gmail.com>
2025-05-08 15:45:42 +0300

    The login for users, by email and password. Test also added. I have not done the testing to check if the email is unique. Feel free to modify and comment.

--- main.cpp
+++ main.cpp
@@ -2,9 +2,37 @@
 // Created by Maroscsak Daniel on 5/8/2025.
 //
 #include <iostream>
+#include "User/UserRepo.h"
+#include "User/UserServices.h"
 using namespace std;
-
+void teste();
 int main() {
-	cout << "End my suffering" << endl;
-    return 0;
+	// cout << "End my suffering" << endl;
+ //    return 0;
+
+	int opt;
+	std::cout << "1. Ruleaza aplicatia\n2. Ruleaza teste\nAlege: ";
+	std::cin >> opt;
+
+	if (opt == 2) {
+		teste();
+		return 0;
+	}
+
+	UserRepo repo;
+	repo.addUser(User("admin@store.com", "parola123", "employee"));
+
+	std::string email, password;
+	std::cout << "Email: ";
+	std::cin >> email;
+	std::cout << "Password: ";
+	std::cin >> password;
+
+	if (AuthService::login(repo, email, password)) {
+		std::cout << "Login successful!\n";
+	} else {
+		std::cout << "Invalid credentials.\n";
+	}
+
+	return 0;
 }
